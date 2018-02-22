f7c412c6667fdd99554387835b0803232dd1897c
JKostov <julijekos@gmail.com>
2018-02-22 21:02:51 +0100

    -This commit will add Admin dashboard + invoices for every building.

--- routes/web.php
+++ routes/web.php
@@ -52,7 +52,8 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
-Route::get('/invoices', 'AdminController@getInvoices')->name('getInvoices');
+//Route::get('/invoices', 'AdminController@getInvoices')->name('getInvoices');
+Route::get('/{building}/invoices', 'AdminController@getInvoices');
 
 Route::get('/registerBuilding', 'BuildingController@getRegisterBuilding')->name('getRegisterBuilding');
 Route::post('/registerBuilding', 'BuildingController@postRegisterBuilding')->name('postRegisterBuilding');
